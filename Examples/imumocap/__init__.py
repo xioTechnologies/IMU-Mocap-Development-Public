5e0f15c19b87934e6e1dcd40ad3575765afea8cd
x-io Technologies <social@x-io.co.uk>
2024-12-20 14:56:58 +0000

    Add Python

---
+++ Examples/imumocap/__init__.py
@@ -0,0 +1,3 @@
+from .link import Link
+from .matrix import Matrix
+from .ui import Axes, Circle, Connection, Dot, Label, Line, link_to_primitives
---
+++ Examples/stream_lower_body.py
@@ -0,0 +1,45 @@
+import hardware
+import imumocap.solvers
+import models
+import time
+
+# Load example model
+model = models.LowerBody()
+
+# Connect to and configure IMUs
+ignored = [
+    model.left_toe.name,
+    model.right_toe.name,
+]  # there are no IMUs on the toes
+
+imus = hardware.setup([l.name for l in model.root.flatten() if l.name not in ignored])
+
+# Stream to UI
+connection = imumocap.ui.Connection()
+
+
+def calibrate():
+    print("Calibrating in...")
+
+    for countdown in [3, 2, 1]:
+        print(countdown)
+
+        time.sleep(0.5)
+
+    imumocap.solvers.calibrate(model.root, {n: i.matrix for n, i in imus.items()})
+
+    print("Calibrated")
+
+
+while True:
+    time.sleep(1 / 30)  # 30 fps
+
+    if any([i.button_pressed for i in imus.values()]):
+        calibrate()
+
+    for name, imu in imus.items():
+        model.root.dictionary()[name].set_joint_from_imu_global(imu.matrix)
+
+    imumocap.solvers.floor(model.root)
+
+    connection.send(imumocap.ui.link_to_primitives(model.root))
