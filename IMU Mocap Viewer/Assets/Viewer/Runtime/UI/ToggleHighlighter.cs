0f6ee976cb41600cbc8e414734681e55bf0a1f67
Phill Tew <peatew@gmail.com>
2025-01-14 13:33:41 +0000

    Consolidated tool-tippleing

--- IMU Mocap Viewer/Assets/Viewer/Runtime/UI/ToggleHighlighter.cs
+++ IMU Mocap Viewer/Assets/Viewer/Runtime/UI/ToggleHighlighter.cs
@@ -7,23 +7,19 @@ using UnityEngine.UI;
 
 namespace Viewer.Runtime.UI
 {
-    [RequireComponent(typeof(Toggle), typeof(RectTransform))]
+    [RequireComponent(typeof(Toggle))]
     public sealed class ToggleHighlighter : MonoBehaviour
     {
         [Header("Color Settings")] [SerializeField]
         private Color normalColor = Color.white;
 
         [SerializeField] private Color highlightColor = Color.yellow;
-        [SerializeField] private string tooltipText = "Tooltip";
-        [SerializeField] private Vector3 tooltipOffset;
         private Toggle toggle;
         private Selectable selectable;
         private ColorBlock colors;
-        private new RectTransform rectTransform;
 
         private void Awake()
         {
-            rectTransform = GetComponent<RectTransform>();
             toggle = GetComponent<Toggle>();
             selectable = GetComponent<Selectable>();
             colors = selectable.colors;
@@ -47,42 +43,6 @@ namespace Viewer.Runtime.UI
             if (toggle == null) return;
 
             toggle.onValueChanged.RemoveListener(OnToggleValueChanged);
-        }
-        
-        
-        void Update()
-        {
-            bool hover = false;
-            
-            if (EventSystem.current.IsPointerOverGameObject())
-            {
-                Vector2 mousePosition = Mouse.current.position.ReadValue();
-                
-                PointerEventData pointerData = new PointerEventData(EventSystem.current)
-                {
-                    position = mousePosition
-                };
-
-                List<RaycastResult> results = new List<RaycastResult>();
-                
-                EventSystem.current.RaycastAll(pointerData, results);
-        
-                foreach (RaycastResult result in results)
-                {
-                    var highlighter = result.gameObject.GetComponentInParent<ToggleHighlighter>();
-                    
-                    if (highlighter == null) continue;
-
-                    if (highlighter.gameObject != gameObject) continue;
-                    
-                    hover = true;
-                    
-                    break;
-                }
-            }
-            
-            if (hover || EventSystem.current.currentSelectedGameObject == toggle.gameObject)
-                Tooltip.ShowTooltip(tooltipText, rectTransform, tooltipOffset);
         }
     }
 }
